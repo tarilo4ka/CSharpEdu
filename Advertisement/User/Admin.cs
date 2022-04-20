40db40fb9a2b06aaca358992f8433fc93e731611
Oleksii Hytsiv <71726515+tarilo4ka@users.noreply.github.com>
2022-04-21 01:53:47 +0300

    delete

--- Advertisement/User/Admin.cs
+++
@@ -1,9 +0,0 @@
-namespace User;
-
-public class Admin : User
-{
-    public Admin(string firstName, string lastName, string email, string password)
-        : base(firstName, lastName, User.RoleInSystem.Admin, email, password)
-    {
-    }
-}
